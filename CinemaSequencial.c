d0ce900ee8f5dd7f92a04e446b5984b2c04562b0
ithonyavila-eng <ithonyavila@gmail.com>
2026-06-10 02:13:36 -0300

    Update CinemaSequencial.c
    last

--- CinemaSequencial.c
+++ CinemaSequencial.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+#include <time.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
 #define LINHAS 7
 #define COLS 5
 
-//IDs das salas
 #define ID_SALA_VIP -1
 #define ID_SALA_NORMAL 1
 
@@ -17,16 +17,13 @@ typedef struct {
     bool possuiReserva;
 } Cinefilo;
 
-//matrizes para mapear salas fisicas do cinema
 int salaVIP[LINHAS][COLS] = {0};
 int salaNormal[LINHAS][COLS] = {0};
 
-//contadores globais de assentos ocupados
 int ocupadosVIP = 0;
 int ocupadosNormal = 0;
 int capacidadeTotal = LINHAS * COLS;
 
-//imprime o estado atual de ocupação de uma sala usando o ID numérico
 void mostrarMapaSala(int idSala) {
     int (*sala)[COLS];
     int ocupados;
@@ -63,10 +60,8 @@ void mostrarMapaSala(int idSala) {
     printf("=========================================\n");
 }
 
-//reserva poltrona aleatoria
 void reservarAssentoAleatorio(int idSala, Cinefilo *cinefilo) {
 
-    //se cinefilo, já tem reserva, acaba execução
     if (cinefilo->possuiReserva){
         printf("Cinéfilo %d já possui uma reserva .\n",cinefilo->id);
         return;
@@ -74,7 +69,6 @@ void reservarAssentoAleatorio(int idSala, Cinefilo *cinefilo) {
     int (*sala)[COLS];
     int *contadorOcupados;
 
-    //define as matrizes da sala e o contador de ocupados
     if (idSala == ID_SALA_VIP){
         sala = salaVIP;
         contadorOcupados = &ocupadosVIP;
@@ -82,31 +76,25 @@ void reservarAssentoAleatorio(int idSala, Cinefilo *cinefilo) {
         sala = salaNormal;
         contadorOcupados = &ocupadosNormal;
     }
-    //verifica se sala está cheia
     if(*contadorOcupados >= capacidadeTotal){
         printf("Sala %d cheia!\n",idSala);
         return;
     }
 
-    //tentativas maximas para cenario de falha
     int tentativas = 0;
     while (tentativas < 50){
         //definindo assentos aleatorios aka posições da matriz
         int l = rand() % LINHAS;
         int c = rand() % COLS;
 
-        //se assento ocupado é igual a 0, significa q o assento está livre
         if(sala[l][c] == 0){
 
-            //atualização de matriz como valor do id do cliente
             sala[l][c] = cinefilo->id;
-            //atualização do struct do cinefilo
             cinefilo->possuiReserva = true;
             cinefilo->salaReservada = idSala;
             cinefilo->linha = l;
             cinefilo->coluna = c;
 
-            //atualização de contador de ocupados
             (*contadorOcupados)++;
 
             printf("[RESERVA]\n" "Cliente : %d\n" "Sala    : %d\n" "Assento : [%d,%d]\n" "Status  : SUCESSO\n\n", cinefilo->id,idSala,l + 1,c + 1);
@@ -129,7 +117,7 @@ void cancelarReserva(Cinefilo *cinefilo) {
     int (*sala)[COLS];
     int *contadorOcupados;
 
-    // Substituído o operador ternário por estrutura if-else
+
     if (cinefilo->salaReservada == ID_SALA_VIP) {
         sala = salaVIP;
         contadorOcupados = &ocupadosVIP;
@@ -138,10 +126,9 @@ void cancelarReserva(Cinefilo *cinefilo) {
         contadorOcupados = &ocupadosNormal;
     }
 
-    //troca valor da matriz por 0
+
     sala[cinefilo->linha][cinefilo->coluna] = 0;
 
-    //atualiza contador
     (*contadorOcupados)--;
 
     printf("[CANCELAMENTO]\n" "Cliente : %d\n" "Sala    : %d\n" "Assento : [%d,%d]\n" "Status  : SUCESSO\n\n",cinefilo->id,cinefilo->salaReservada, cinefilo->linha + 1,cinefilo->coluna + 1);
@@ -150,7 +137,6 @@ void cancelarReserva(Cinefilo *cinefilo) {
     cinefilo->possuiReserva = false;
 }
 
-//validar a consistência matemática comparando os contadores com as matrizes
 void verifica_integridade() {
     int somaVIP = 0;
     int somaNormal = 0;
@@ -163,7 +149,7 @@ void verifica_integridade() {
     }
 
     printf("\n[AUDITORIA DE INTEGRIDADE]\n");
-    printf("Sala VIP (ID: %d)    -> Contador: %d | Fisico na Matriz: %d -> %s\n", ID_SALA_VIP, ocupadosVIP, somaVIP, (ocupadosVIP == somaVIP) ? "OK" : "CORROMPIDO");
+    printf("Sala VIP    (ID:%d) -> Contador: %d | Fisico na Matriz: %d -> %s\n", ID_SALA_VIP, ocupadosVIP, somaVIP, (ocupadosVIP == somaVIP) ? "OK" : "CORROMPIDO");
     printf("Sala Normal (ID: %d) -> Contador: %d | Fisico na Matriz: %d -> %s\n", ID_SALA_NORMAL, ocupadosNormal, somaNormal, (ocupadosNormal == somaNormal) ? "OK" : "CORROMPIDO");
 }
 
@@ -185,7 +171,7 @@ void acaoClienteSimulado(Cinefilo *cinefilo) {
 
 int main() {
 
-    srand(time(NULL));
+    srand(42);
 
     int totalClientesParaTestar = 10;
 
@@ -204,6 +190,7 @@ int main() {
     printf(" SISTEMA DE BILHETERIA - MODO SEQUENCIAL\n");
     printf("=========================================\n");
 
+    __clock_t inicio = clock();
     for (int i = 0; i < totalClientesParaTestar; i++) {
 
         printf("\n-----------------------------------------\n");
@@ -213,6 +200,9 @@ int main() {
         acaoClienteSimulado(&clientes[i]);
     }
 
+    __clock_t fim = clock();
+
+    double tempoTotal = ((double)(fim-inicio))/CLOCKS_PER_SEC;
     printf("\n\n=========================================\n");
     printf("     FIM DA SIMULAÇÃO SEQUENCIAL\n");
     printf("=========================================\n");
@@ -221,6 +211,8 @@ int main() {
     mostrarMapaSala(ID_SALA_NORMAL);
 
     verifica_integridade();
+
+    printf("Tempo de execução: %.6f segundos\n", tempoTotal);
 
     return 0;
 }
